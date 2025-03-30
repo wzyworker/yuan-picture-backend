7c657708d68e257eb2804e2af93663c1538df62d
wzyworker <wzy@hfut.com>
2025-03-30 09:30:20 +0800

    用户模块三，添加分页插件，解决Long类型转json精度丢失问题

---
+++ src/main/java/com/wzy/yuanpicturebackend/config/JsonConfig.java
@@ -0,0 +1,31 @@
+package com.wzy.yuanpicturebackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @author wzy
+ * @date 2025年03月30日 9:23
+ */
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 解决Long类型转Json类型精度丢失的问题
+     * @param builder
+     * @return
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
